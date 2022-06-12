41647a977b8b166712846c192179575ce4128d0f
Valentín Piantanida <valenpiantanida@gmail.com>
2022-06-12 20:12:47 -0300

    subo ejercicio5 hecho para todos los casos

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -12,42 +12,31 @@ def cadena_balanceada(cadena):
     """Esta función recibe una cadena de texto y devuelve un valor True
     si cada corchete que abre tiene su par que cierra, y false si no es así
     """
-    corchete_abierto = 0
-    corchete_cerrado = 0
-    lista = []
-    contador = 0
-    for i in cadena:
-        if cadena[contador] == "[":
-            corchete_abierto += 1
-            lista.append(i)
-            contador += 1
-        elif cadena[contador] == "]":
-            corchete_cerrado += 1
-            lista.append(i)
-            contador += 1
-        else:
-            contador += 1
-    if corchete_abierto == corchete_cerrado:
-        contador = 0
-        for c in lista:
-            if lista[contador] == "[":
-                contador += 1
-                return True
-            elif lista[contador] == "]":
-                contador += 1
+    pila = []
+    for c in cadena:
+        if c == "[":
+            pila.append(c)
+        elif c == "]":
+            if len(pila) == 0:
                 return False
-        return lista
-    else:
-        return False  
+            else:
+                pila_top = pila.pop()
+                if pila_top != "[":
+                    return False
+    print(len(pila))
+    return not len(pila) 
         
 def principal():
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
-    lista = "[]][[]]"
+    lista = input("Ingrese la cadena de texto: ")
     resultado = cadena_balanceada(lista)
-    print(resultado)
+    if resultado:
+        print("Esta balanceada")
+    else:
+        print("No está balanceada")
     
 
 if __name__ == "__main__":
